0231841260637534cf8b1d3b110ce7135c8e78d5
Sashka IOS Developer <dam@kharchenko.home>
2022-07-03 16:01:12 +0200

    Three commit

---
+++ Ekran/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Ekran
+//
+//  Created by Oleksandr on 13.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var fakeButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+        self.view.backgroundColor = .yellow
+        
+        if fakeButton.backgroundColor == .red {
+            fakeButton.backgroundColor = .clear
+        } else {
+            fakeButton.backgroundColor = .red
+
+        
+    }
+
+}
+}
+ one F_GETOWN
